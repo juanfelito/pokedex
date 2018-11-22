906e1d6b62b258ede6e27d97e52d50e83f2bc43f
Felipe <felipe.balcero45@com>
2018-11-22 17:54:44 -0500

    Changing computer commit

--- src/components/Pokemon-list-item.vue
+++ src/components/Pokemon-list-item.vue
@@ -8,43 +8,44 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    data () {
-        return {
-            greeting: 'Hello',
-            name: '',
-            urlImagen: '',
-            errors: []
-        }
-    },
-    filters:{
-        iniciaConMayuscula: function(valor) {
-            return valor.charAt(0).toUpperCase() + valor.slice(1);
-        }
-    },
-    props: ['id'],
-    mounted () {
-        axios
-            .get('https://pokeapi.co/api/v2/pokemon/'+this.id+'/')
-            .then(response => {
-                this.name = response.data.name;
-                this.urlImagen = response.data.sprites.front_default;
-            })
-            .catch(e => {
-                this.errors.push(e);
-            })
+  data () {
+    return {
+        greeting: 'Hello',
+        name: '',
+        urlImagen: '',
+        errors: []
     }
-}
+  },
+  filters:{
+    iniciaConMayuscula: function(valor) {
+        return valor.charAt(0).toUpperCase() + valor.slice(1);
+    }
+  },
+  props: ['id'],
+  mounted () {
+    axios
+        .get('https://pokeapi.co/api/v2/pokemon/'+this.id+'/')
+        .then(response => {
+            this.name = response.data.name;
+            this.urlImagen = response.data.sprites.front_default;
+        })
+        .catch(e => {
+            this.errors.push(e);
+        })
+  }
+};
 </script>
 
 <style lang="scss">
 .pokemon-list-item {
   padding: 10px;
-  width: 100px;
+  width: 150px;
   float: left;
   margin: 2px;
   background: #eee;
+  cursor: pointer;
 }
 </style>
